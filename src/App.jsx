4de67d16562a7d6abd10baa55a9691eb27da16e4
gallaDeepesh <galladeepesh@gmail.com>
2026-02-11 19:16:29 +0530

    modifies app.jsx_1

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import './components/Admin.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StudentScanner from "./components/StudentScanner.jsx";
 import AdminQR from "./components/Admin.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/admin" element={<AdminQR />} />
         <Route path="/student" element={<StudentScanner />} />
       </Routes>
